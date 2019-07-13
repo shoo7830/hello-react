d4b499a5be307e13551f2b08f2dc40b0ec12accc
Aria Kim <shoo7830@gmail.com>
2019-07-13 22:24:42 +0900

    react study 03

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import MyComponent from './MyComponent';
+
+class App extends Component {
+  render() {
+    return (
+        <MyComponent name="shoo7830" age={42} />
+    );
+  }
+}
+
+export default App;
